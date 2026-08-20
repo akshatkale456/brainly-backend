848a443f16b0060c9ce262ea2f0789972d6a972b
akshatkale456 <akshatkale02@gmail.com>
2026-08-20 11:00:16 +0530

    change cors url production to local

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-import app from './app.js';import http from "http";import cors from 'cors';import { initializewebsocketserver } from './ws/ws.js';const server = http.createServer(app);initializewebsocketserver(server);app.use(cors({  origin: 'http://localhost:5173',   methods: ['GET', 'POST','DELETE','UPDATE'],  credentials: true}));const PORT = 3000;server.listen(PORT, () => {  console.log(`Server is running on http://localhost:${PORT}`);});
+import app from './app.js';
+import http from "http";
+import cors from 'cors';
+import { initializewebsocketserver } from './ws/ws.js';
+const server = http.createServer(app);
+initializewebsocketserver(server);
+app.use(cors({
+  origin: 'https://brainly-8la7wdl9y-brainly1.vercel.app/', 
+  methods: ['GET', 'POST','DELETE','UPDATE'],
+  credentials: true
+}));
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
